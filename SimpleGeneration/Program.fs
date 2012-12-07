07b27333151139ca23f47e3dc6de29fa0830e085
mglunafh <ufispb@gmail.com>
2012-12-07 04:28:15 +0300

    two new functions, 'append' and 'insert', which append/insert a string to/into the given System.Text.StringBuilder

--- SimpleGeneration/Program.fs
+++ SimpleGeneration/Program.fs
@@ -19,49 +19,52 @@ let writeToFile fn (str : string) =
    use f = File.CreateText fn
    f.WriteLine(str)
 
+let append (stringBuilder : System.Text.StringBuilder) (str : string) = ignore(stringBuilder.Append(str))
+let insert (stringBuilder : System.Text.StringBuilder) position (str : string) = ignore(stringBuilder.Insert(position, str))
+
 let activities = new System.Text.StringBuilder()
 
 let manifest = new System.Text.StringBuilder()
-ignore(manifest.Append("<?xml version=\"1.0\" encoding=\"utf-8\"?>
+append manifest ("<?xml version=\"1.0\" encoding=\"utf-8\"?>
 <manifest xmlns:android=\"http://schemas.android.com/apk/res/android\"
     package=\"com.qrealclouds." + package + "\"
     android:versionCode=\"1\"
     android:versionName=\"1.0\">
 
-    <uses-sdk android:minSdkVersion=\"8\"/>\n"))
+    <uses-sdk android:minSdkVersion=\"8\"/>\n")
 
 let createImplementation form =
     if (form = "main") then
-        ignore(activities.Append("        <activity android:name=\".main\"
+        append activities ("        <activity android:name=\".main\"
             android:label=\"@string/app_name\">
             <intent-filter>
                 <action android:name=\"android.intent.action.MAIN\"/>
                 <category android:name=\"android.intent.category.LAUNCHER\"/>
             </intent-filter>
-        </activity>"))
+        </activity>")
     else
-        ignore(activities.Append("\n        <activity android:name=\"." + form + "\"></activity>"))
+        append activities ("\n        <activity android:name=\"." + form + "\"></activity>")
 
     // imports register
     let imports = new Hashtable()
 
     let currentImports = new System.Text.StringBuilder()
-    ignore(currentImports.Append("\nimport android.app.Activity;
+    append currentImports ("\nimport android.app.Activity;
 import android.os.Bundle;
-import android.view.View;\n"))
+import android.view.View;\n")
 
     let onCreate = new System.Text.StringBuilder()
-    ignore(onCreate.Append("\n\n    /**
+    append onCreate ("\n\n    /**
     * Called when the activity is first created.
     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout." + form + ");"))
+        setContentView(R.layout." + form + ");")
             
     let activity = System.Text.StringBuilder()
-    ignore(activity.Append(currentImports.ToString()))
-    ignore(activity.Append("\npublic class " + form + " extends Activity {"))
+    append activity (currentImports.ToString())
+    append activity ("\npublic class " + form + " extends Activity {")
 
     let reader = XmlReader.Create(path + @"\res\layout\" + form + ".xml")
     while (reader.Read ()) do
@@ -69,10 +72,10 @@ import android.view.View;\n"))
             let onClickName = reader.GetAttribute("android:onClick")
 
             if not (imports.ContainsKey("android.content.Intent")) then 
-                ignore(activity.Insert(0, "\nimport android.content.Intent;"))
+                insert activity 0 "\nimport android.content.Intent;"
                 imports.Add("android.content.Intent", "android.content.Intent")
 
-            ignore(activity.Append("\n\n    public void " + onClickName + "(View v) {"))
+            append activity ("\n\n    public void " + onClickName + "(View v) {")
 
             let readerTrans = XmlReader.Create(path + @"\Transition2.xml")
             let id = reader.GetAttribute("android:id")
@@ -82,29 +85,29 @@ import android.view.View;\n"))
 
             let nextForm = readerTrans.GetAttribute("name_to")
 
-            ignore(activity.Append("
+            append activity ("
         Intent intent = new Intent(this, " + nextForm + ".class);
-        startActivity(intent);"))
-            ignore(activity.Append("\n    }"))
+        startActivity(intent);")
+            append activity "\n    }"
         elif reader.Name = "WebView" then
             if not (permissions.ContainsKey("Internet")) then 
-                ignore(manifest.Append("    <uses-permission android:name=\"android.permission.INTERNET\" />\n"))
+                append manifest "    <uses-permission android:name=\"android.permission.INTERNET\" />\n"
                 permissions.Add("Internet", "Internet")
 
             if not (imports.ContainsKey("android.webkit.WebView")) then 
-                ignore(activity.Insert(0, "\nimport android.webkit.WebView;"))
+                insert activity 0 "\nimport android.webkit.WebView;"
                 imports.Add("android.webkit.WebView", "android.webkit.WebView")
 
-            ignore(onCreate.Append("\n        WebView webView = (WebView) findViewById(R.id.webViewInfo);
+            append onCreate "\n        WebView webView = (WebView) findViewById(R.id.webViewInfo);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl(\"http://www.lanit-tercom.ru\");")) // сайт зашит в код
+        webView.loadUrl(\"http://www.lanit-tercom.ru\");" // сайт зашит в код
 
-    ignore(onCreate.Append("\n    }"))
-    ignore(activity.Append(onCreate.ToString()))
+    append onCreate "\n    }"
+    append activity (onCreate.ToString())
 
-    ignore(activity.Append("\n}"))
+    append activity "\n}"
 
-    ignore(activity.Insert(0, "package com.qrealclouds." + package + ";\n"))
+    insert activity 0 "package com.qrealclouds." + package + ";\n"
 
     writeToFile (path + @"\src\com\qrealclouds\" + package + "\\" + form + ".java") (activity.ToString())
 
@@ -120,9 +123,9 @@ createImplementation "seventh"
 createImplementation "eighth"
 createImplementation "ninth"
 
-ignore(manifest.Append("\n    <application android:label=\"@string/app_name\"
-        android:theme=\"@android:style/Theme.Light.NoTitleBar\">\n"))
-ignore(manifest.Append(activities.ToString()))
-ignore(manifest.Append("\n    </application>
-</manifest>"))
+append manifest ("\n    <application android:label=\"@string/app_name\"
+        android:theme=\"@android:style/Theme.Light.NoTitleBar\">\n")
+append manifest (activities.ToString())
+append manifest ("\n    </application>
+</manifest>")
 writeToFile (path + "\AndroidManifest.xml") (manifest.ToString())
